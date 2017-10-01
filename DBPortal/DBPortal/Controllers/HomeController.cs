0f919c234a00fb9695e3257109d85afc9cf612a7
Vijay <gudevijay@gmail.com>
2017-10-01 12:46:00 +0530

    Search text Related Changes Done.

--- DBPortal/DBPortal/Controllers/HomeController.cs
+++ DBPortal/DBPortal/Controllers/HomeController.cs
@@ -42,5 +42,19 @@ namespace DBPortal.Controllers
 
             return View(inventory);
         }
+
+        public ActionResult SearchInventory(string searchWord)
+        {
+            var inventory = new List<InventoryModel>();
+
+            var invData = DBServices.GetTblView("exce sampleprocedure(" + searchWord + ")");
+
+            if (invData.Count > 0)
+            {
+                inventory = DBServices.ToList<InventoryModel>(invData);
+            }
+
+            return PartialView("_InvData", inventory);
+        }
     }
 }
